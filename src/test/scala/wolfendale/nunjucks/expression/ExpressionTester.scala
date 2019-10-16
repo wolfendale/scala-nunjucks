d009831af4fb0986cebc5eda8244f37aa1d89860
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2019-10-16 11:59:17 +0100

    Make sure the expression tester attempts to parse the whole string

--- src/test/scala/wolfendale/nunjucks/expression/ExpressionTester.scala
+++ src/test/scala/wolfendale/nunjucks/expression/ExpressionTester.scala
@@ -7,6 +7,10 @@ class ExpressionTester(environment: Environment = new ProvidedEnvironment()) {
 
   def evaluate(expression: String, scope: Value.Obj = Value.Obj.empty): Value = {
     import fastparse._
-    parse(expression, Parser.expression(_)).get.value.eval(Context(environment, Frame(scope)))
+    import SingleLineWhitespace._
+
+    def parser[_: P] = P(Parser.expression ~ End)
+
+    parse(expression, parser(_)).get.value.eval(Context(environment, Frame(scope)))
   }
 }
