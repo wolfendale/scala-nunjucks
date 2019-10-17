3ff089664b8b72b38bf68bcd5fcf09dae1fa64a5
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2019-10-17 21:17:34 +0100

    Add HTML escaping

--- src/main/scala/wolfendale/nunjucks/filters/package.scala
+++ src/main/scala/wolfendale/nunjucks/filters/package.scala
@@ -46,7 +46,7 @@ package object filters {
       case arr: Arr =>
         arr.destructure.headOption.getOrElse(Undefined)
       case str: Str =>
-        str.value.headOption.map(_.toString).map(Str.apply).getOrElse(Undefined)
+        str.value.headOption.map(_.toString).map(Str(_)).getOrElse(Undefined)
       case Infinity | `-Infinity` =>
         Undefined
       case Value.Obj(_) | Number(_) | Value.True | Value.False | NaN =>
