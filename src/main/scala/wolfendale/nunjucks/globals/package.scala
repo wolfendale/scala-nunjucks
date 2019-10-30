dcec57ed09260056ba90dfbc35056bdd1bc4ab8b
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2019-10-30 13:32:57 +0000

    range: Implement range spec - needs tests for coercion

--- src/main/scala/wolfendale/nunjucks/globals/package.scala
+++ src/main/scala/wolfendale/nunjucks/globals/package.scala
@@ -4,6 +4,7 @@ import wolfendale.nunjucks.expression.runtime.Value
 import cats._
 import cats.data._
 import cats.implicits._
+import wolfendale.nunjucks.expression.runtime.Value.{Arr, Number}
 
 package object globals {
 
@@ -24,7 +25,20 @@ package object globals {
     }
   }
 
+  val range: Value.Function = Value.Function{ parameters =>
+    val (start, stop, step):(Value.Number, Value.Number, Value.Number) = parameters.parameters.length match {
+      case 1 => (Number(0), parameters.get(0).get.asInstanceOf[Number], Number(1))
+      case 2 => (parameters.get(0).get.asInstanceOf[Number], parameters.get(1).get.asInstanceOf[Number], Number(1))
+      case 3 => (parameters.get(0).get.asInstanceOf[Number], parameters.get(1).get.asInstanceOf[Number], parameters.get(2).get.asInstanceOf[Number])
+    }
+
+    State.pure {
+      Arr.from((start.value until stop.value by step.value).map(Number):_*)
+    }
+  }
+
   lazy val defaults: Map[String, Value] = Map(
-    "joiner" -> joiner
+    "joiner" -> joiner,
+    "range"  -> range
   )
 }
