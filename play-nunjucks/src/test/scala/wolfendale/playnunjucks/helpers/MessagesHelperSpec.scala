d3e36d65fa363daf5924959367fda440381374d8
Chris Wright <chrisjameswright@users.noreply.github.com>
2019-11-25 13:57:29 +0000

    Refactoring MessagesHelperSpec to use templates

--- play-nunjucks/src/test/scala/wolfendale/playnunjucks/helpers/MessagesHelperSpec.scala
+++ play-nunjucks/src/test/scala/wolfendale/playnunjucks/helpers/MessagesHelperSpec.scala
@@ -2,9 +2,7 @@ package wolfendale.playnunjucks.helpers
 
 import org.scalatest.{FreeSpec, MustMatchers}
 import play.api.test.{FakeRequest, Helpers}
-import wolfendale.nunjucks.expression.runtime.Value
-import wolfendale.nunjucks.expression.runtime.Value.Function.{Parameter, Parameters}
-import wolfendale.nunjucks.{Context, ProvidedEnvironment, RenderMode}
+import wolfendale.nunjucks.ProvidedEnvironment
 
 class MessagesHelperSpec extends FreeSpec with MustMatchers {
 
@@ -18,81 +16,42 @@ class MessagesHelperSpec extends FreeSpec with MustMatchers {
     )
   )
   private val helper   = new MessagesHelper(messages)
-  private val function = helper.value(FakeRequest()).asInstanceOf[Value.Function]
-  private val ctx      = Context(new ProvidedEnvironment(), RenderMode.Template)
-
-  def runHelper(params: Parameters): String =
-    function.fn(params).runA(ctx).value.toStr.value
+  private val nunjucks = new ProvidedEnvironment().addGlobal("messages", helper.value(FakeRequest()))
 
   "MessagesHelper" - {
 
     "should render valid message keys" in {
-      val params = Parameters(
-        Seq(
-          Parameter(Value.Str("key"))
-        ))
-
-      val result = runHelper(params)
+      val result = nunjucks.render("{{ messages('key') }}")
 
       result mustBe "hello"
     }
 
     "should render valid message keys with parameters" in {
-      val params = Parameters(
-        Seq(
-          Parameter(Value.Str("keyParams")),
-          Parameter(Value.Str("world"))
-        ))
-
-      val result = runHelper(params)
+      val result = nunjucks.render("{{ messages('keyParams', 'world') }}")
 
       result mustBe "hello world"
     }
 
     "should render valid message keys with multiple parameters" in {
-      val params = Parameters(
-        Seq(
-          Parameter(Value.Str("keyMultiParams")),
-          Parameter(Value.Str("world")),
-          Parameter(Value.Str("!"))
-        ))
-
-      val result = runHelper(params)
+      val result = nunjucks.render("{{ messages('keyMultiParams', 'world', '!') }}")
 
       result mustBe "hello world!"
     }
 
     "should render valid message keys with missing parameters" in {
-      val params = Parameters(
-        Seq(
-          Parameter(Value.Str("keyParams"))
-        ))
-
-      val result = runHelper(params)
+      val result = nunjucks.render("{{ messages('keyParams') }}")
 
       result mustBe "hello {0}"
     }
 
     "should render valid message keys with excess parameters" in {
-      val params = Parameters(
-        Seq(
-          Parameter(Value.Str("keyParams")),
-          Parameter(Value.Str("world")),
-          Parameter(Value.Str("!"))
-        ))
-
-      val result = runHelper(params)
+      val result = nunjucks.render("{{ messages('keyParams', 'world', '!') }}")
 
       result mustBe "hello world"
     }
 
     "should render invalid message keys as the key" in {
-      val params = Parameters(
-        Seq(
-          Parameter(Value.Str("missingKey"))
-        ))
-
-      val result = runHelper(params)
+      val result = nunjucks.render("{{ messages('missingKey') }}")
 
       result mustBe "missingKey"
     }
